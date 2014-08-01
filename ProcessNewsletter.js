540656bbd1d19dda61585620bb4ab8dace345fca
Nico Knoll <mail@nico-knoll.de>
2014-08-01 11:11:40 +0200

    1.0.0: Initial release

---
+++ ProcessNewsletter.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$("table.AdminDataTableSortable").tablesorter({headers:{0:{sorter:false}}}); 
+	
+	$('table.AdminDataTableSortable').on('click', '.toggle_all', function(e){
+		el = $(this);
+		if(this.checked) {
+			// Iterate each checkbox
+			$(':checkbox').each(function() {
+				$(this).attr('checked', true);                      
+			});
+			
+			setTimeout(function() {
+				el.attr('checked', true);
+			}, 10);
+		} else {
+			$(':checkbox').each(function() {
+				$(this).attr('checked', false);                      
+			});
+			
+			setTimeout(function() {
+				el.attr('checked', false);
+			}, 10);
+		}
+	});
+});
