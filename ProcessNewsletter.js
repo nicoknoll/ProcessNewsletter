763dc87100456d0fbf66496eb2764aa0d517e260
Nico Knoll <mail@nico-knoll.de>
2014-08-01 12:49:59 +0200

    Minor changes

--- ProcessNewsletter.js
+++ ProcessNewsletter.js
@@ -1,25 +1,27 @@
 $(document).ready(function(){
-	$("table.AdminDataTableSortable").tablesorter({headers:{0:{sorter:false}}}); 
-	
-	$('table.AdminDataTableSortable').on('click', '.toggle_all', function(e){
-		el = $(this);
-		if(this.checked) {
-			// Iterate each checkbox
-			$(':checkbox').each(function() {
-				$(this).attr('checked', true);                      
-			});
-			
-			setTimeout(function() {
-				el.attr('checked', true);
-			}, 10);
-		} else {
-			$(':checkbox').each(function() {
-				$(this).attr('checked', false);                      
-			});
-			
-			setTimeout(function() {
-				el.attr('checked', false);
-			}, 10);
-		}
-	});
+	if($("table.AdminDataTableSortable").length > 0) {
+		$("table.AdminDataTableSortable").tablesorter({headers:{0:{sorter:false}}}); 
+		
+		$('table.AdminDataTableSortable').on('click', '.toggle_all', function(e){
+			el = $(this);
+			if(this.checked) {
+				// Iterate each checkbox
+				$(':checkbox').each(function() {
+					$(this).attr('checked', true);                      
+				});
+				
+				setTimeout(function() {
+					el.attr('checked', true);
+				}, 10);
+			} else {
+				$(':checkbox').each(function() {
+					$(this).attr('checked', false);                      
+				});
+				
+				setTimeout(function() {
+					el.attr('checked', false);
+				}, 10);
+			}
+		});
+	}
 });
